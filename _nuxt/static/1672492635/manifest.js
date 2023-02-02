d9d84f50f60a89bcf0af92f32a89e2ccb9ce6c75
Joshua Adewole <adewole.josh@gmail.com>
2023-02-02 17:18:51 +0100

    Updates

--- _nuxt/static/1672492635/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprojects","\u002Fblog","\u002Fabout","\u002Fblog\u002Fthat-year","\u002Fblog\u002Fa-new-beginning"]})
---
+++ _nuxt/static/1675354616/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675354616",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"30a2bd51"}}}}(null));
